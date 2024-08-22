0e41dae9d5ab01f8d95fc36f50500b034de2571c
Syed Safdar Ali Shah <programmersafdar@live.com>
2024-08-22 10:18:12 +0500

    sanity added to the project

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="px-4 py-5 w-full relative max-w-2xl mx-auto flex items-center justify-between">
+      <Link href="/" className="font-bold text-3xl">
+        Developers<span className="text-blue-500">Blog</span>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
